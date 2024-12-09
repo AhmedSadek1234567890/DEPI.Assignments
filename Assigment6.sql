18b19fe00f031d6c94e728f3e610f291f6a75a71
Ahmed Elsadek <156125842+AhmedSadek1234567890@users.noreply.github.com>
2024-12-09 04:12:28 +0200

    Add files via upload

---
+++ Assigment6.sql
@@ -0,0 +1,28 @@
+/*Task 1: Using Comparison and Logical Operators*/
+/*Question:
+Write a SQL query to retrieve the emp_id , last_name , and salary of employees whose salary is
+between 2,000 and 5,000 and do not have a manager ID of 101 or 200.*/
+SELECT last_name, salary
+FROM employees
+WHERE salary BETWEEN 2000 AND 5000
+  AND manager_id NOT IN (101, 200);
+
+
+/*Task 2: Using JOINs and Aliases*/
+/*Write a SQL query to display the employee names along with their respective department names.
+Use aliases for table names for better readability*/
+SELECT e.first_name AS employee_name, d.name AS department_name
+FROM employees e
+INNER JOIN departments d
+  ON e.department_id = d.department_id
+ORDER BY d.name ASC;
+
+
+
+/*Task 3: Aggregate Functions and GROUP BY
+Question:
+Write a SQL query to find the number of employees and the average salary for each department.
+Ensure that the results are grouped by department ID.*/
+SELECT department_id, COUNT(*) AS num_employees, AVG(salary) AS avg_salary
+FROM employees
+GROUP BY department_id;
